83a2cccc01de33eff56a3b67f7d6be3e5a9e5c47
BOTOND-SZABOLCS BARTHA <botond.szabolcs.bartha@stud.ubbcluj.ro>
2024-12-15 22:15:59 +0200

    Laborator9 finalizat cu succes

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,15 +3,15 @@ namespace BarthaBotondLab7
 {
     public partial class App : Application
     {
-        static ShoppingListDatabase database;
-        public static ShoppingListDatabase Database
+        static ShopListDatabase database;
+        public static ShopListDatabase Database
         {
             get
             {
                 if (database == null)
                 {
                     database = new
-                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   ShopListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
                    LocalApplicationData), "ShoppingList.db3"));
                 }
                 return database;
