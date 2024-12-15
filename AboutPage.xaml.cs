553ad9ae5936feebe617157699678f0e906bb888
BOTOND-SZABOLCS BARTHA <botond.szabolcs.bartha@stud.ubbcluj.ro>
2024-12-15 17:15:00 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BarthaBotondLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BarthaBotondLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
